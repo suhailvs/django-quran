c26529ba2332c9dada5ec4e2a2d8f9b49318a75d
suhailvs <suhailvs@gmail.com>
2015-04-12 00:58:35 +0530

    added aisha_bewley translation

--- sample/myapp/urls.py
+++ sample/myapp/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import *
 urlpatterns = patterns('myapp.views',
     # Examples:
     url(r'^$', 'index', name='quran_index'),
-    url(r'^(?P<sura_number>\d+)/$', 'sura', name='quran_sura'),
+    url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/$', 'aya', name='quran_aya'),
 
-    # Parts : 
-	url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/$', 'aya', name='quran_aya'),
+    # sura
+    url(r'^(?P<sura_number>\d+)/(?P<aya_range>.+/)?$', 'sura', name='quran_sura'),
+
+    # Parts : 	
     url(r'^(?P<sura_number>\d+)/(?P<aya_number>\d+)/(?P<word_number>\d+)/$', 'word', name='quran_word'),
     url(r'^lemma/(?P<lemma_id>\d+)/$', 'lemma', name='quran_lemma'),
     url(r'^root/(?P<root_id>\d+)/$', 'root', name='quran_root'),
--- sample/myapp/views.py
+++ sample/myapp/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404,get_list_or_404
 from quran.models import *
+import re
 # Create your views here.
 def index(request):
     suras = get_list_or_404(Sura)
     return render(request,'index.html', {'suras': [suras[:38],suras[38:76],suras[76:]]})
 
-def sura(request, sura_number):
+def sura(request, sura_number,aya_range=None):        
     cur_trans = int(request.GET.get('trans',1))
     sura = get_object_or_404(Sura, number=sura_number)
     ayas = sura.ayas.all()
     trans = get_list_or_404(QuranTranslation)
     translations = sura.translations.filter(translation__id=cur_trans)
+    if aya_range:
+        m = re.match('(\d{1,3}).(\d{1,3})',aya_range)
+        if m: 
+            l,u = m.group(1),m.group(2)
+            u = u if u > l else l # cases like 12-1
+            
+            ayas = ayas.filter(number__range=[l,u])
+            translations = translations.filter(aya__range=[l,u])
+
     ayas = zip(ayas, translations)
     return render(request,'sura.html',  {'sura': sura, 'ayas': ayas,'trans':trans,'cur_trans':cur_trans})
 
