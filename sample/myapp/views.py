179b1b8c8e6d1b1d470b39178882b06185475eaa
suhailvs <suhailvs@gmail.com>
2015-04-15 18:27:31 +0530

    updated the css and fixed some bugs

--- sample/myapp/views.py
+++ sample/myapp/views.py
@@ -6,6 +6,10 @@ def index(request):
     suras = get_list_or_404(Sura)
     return render(request,'index.html', {'suras': [suras[:38],suras[38:76],suras[76:]]})
 
+def root_index(request):
+    roots = Root.objects.all().order_by('letters')
+    return render(request,'root_index.html', {'roots': roots})
+    
 def sura(request, sura_number,aya_range=None):        
     cur_trans = int(request.GET.get('trans',1))
     sura = get_object_or_404(Sura, number=sura_number)
@@ -20,20 +24,19 @@ def sura(request, sura_number,aya_range=None):
             ayas = ayas.filter(number__range=[l,u])
             translations = translations.filter(aya__number__range=[l,u])
     ayas = zip(ayas, translations)
-
     return render(request,'sura.html',  {'sura': sura, 'ayas': ayas,'trans':trans,'cur_trans':cur_trans})
 
 
-###############################
-####         PARTS        #####
-###############################
 def aya(request, sura_number, aya_number, translation=1):
     sura = get_object_or_404(Sura, number=sura_number)
     aya = get_object_or_404(Aya, sura=sura, number=aya_number)
     translated_aya = get_object_or_404(TranslatedAya, aya=aya, translation=translation)
     words = aya.words.all()
-    return render(request,'parts/aya.html', {'sura': sura, 'aya': aya, 'translation': translated_aya, 'words': words})
+    return render(request,'aya.html', {'sura': sura, 'aya': aya, 'translation': translated_aya, 'words': words})
 
+##########################################
+####         PARTS OF PAGE AYA       #####
+##########################################
 def word(request, sura_number, aya_number, word_number):
     aya = get_object_or_404(Aya, sura=sura_number, number=aya_number)
     word = get_object_or_404(Word, aya=aya, number=word_number)
@@ -52,7 +55,3 @@ def root(request, root_id):
     lemmas = root.lemmas.all()
     ayas = root.ayas.distinct()
     return render(request,'parts/root.html', {'root': root, 'lemmas': lemmas, 'ayas': ayas})
-
-def root_index(request):
-    roots = Root.objects.all().order_by('letters')
-    return render(request,'parts/root_index.html', {'roots': roots})
